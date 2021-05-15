b57f720ffd0e58ec1ae45569d69ae159e919c938
dank1 <kirshendan@gmail.com>
2021-05-15 18:59:25 +0300

    _

---
+++ functions.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+def squared_error_derivative(output, label):
+    res = output - label
+    return res
+
+
+def binary_cross_entropy_derivative(output, label):
+    res = -label/output + ((1 - label)/(1 - output))
+    return res
+
+
+def sigmoid_derivative(z):
+    res = z * (1 - z)
+    return res
+
+
+def relu_derivative(z):
+    res = z
+    res[res < 0] = 0
+    res[res > 0] = 1
+    res[res == 0] = 0.5
+    return res
+
+
+def sigmoid(z):
+    res = 1/(1 + np.exp(-z))
+    return res
+
+
+def relu(z):
+    res = z
+    res[res < 0] = 0
+    return res
+
+
+activation_function_dispatcher = {'sigmoid': sigmoid,
+                                  'relu': relu}
+
+activation_function_derivative_dispatcher = {'sigmoid': sigmoid_derivative,
+                                             'relu': relu_derivative}
+
+loss_function_derivative_dispatcher = {'se': squared_error_derivative,
+                                       'bce': binary_cross_entropy_derivative}
